537abcaa2c22ef2766db5ed96eda7269ac5971e9
Adrian Smith <fon@thefon.net>
2017-03-14 01:09:27 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+This is a scraper that runs on [Morph](https://morph.io) for City of Bayswater Development Applications. It scrapes http://engage.bayswater.wa.gov.au/planning-applications-public-advertising
+
+To get started [see the documentation](https://morph.io/documentation)
--- scraper.py
+++ scraper.py
@@ -1,24 +1,67 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python3
+"""
+This is a template for a Python scraper on morph.io (https://morph.io)
+including some code snippets below that you should find helpful
 
+Based on https://github.com/planningalerts-scrapers/noosa_council
+"""
+import os
+import sys
 # import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import lxml.html
+import requests
+from morph_planningalerts import DevelopmentApplication, MorphDatabase
+
+DEFAULT_START_URL = "http://engage.bayswater.wa.gov.au/planning-applications-public-advertising"
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+def main(url):
+    MorphDatabase.init()
+
+    html = requests.get(url)
+    tree = lxml.html.fromstring(html.content)
+    print(tree)
+    sys.exit(0)
+
+    count_new = total = 0
+    for application_url in get_application_links(url):
+
+        if not application_url:
+            # Skipped entry...
+            total += 1
+            continue
+
+        # html = scraperwiki.scrape(url)
+
+        # XPath
+        #This will create a list of buyers:
+        buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+        #This will create a list of prices
+        prices = tree.xpath('//span[@class="item-price"]/text()')
+
+        # Find something on the page using css selectors
+        # root.cssselect("div[align='left']")
+        #
+        data = extract_application_details(application_url)
+
+        application, created = DevelopmentApplication.get_or_create(**data)
+
+        total += 1
+
+        if not created:
+            print("* Skipping {0.council_reference}".format(application))
+        else:
+            print("Saved {0.council_reference}".format(application))
+            count_new += 1
+
+    print("Added {0} records out of {1} processed.".format(count_new, total))
+
+
+if __name__ == "__main__":
+    main(os.environ.get('MORPH_START_URL', DEFAULT_START_URL))
+
